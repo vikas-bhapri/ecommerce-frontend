46d82ebab8ab45a0ee34a81161baff1c745bed17
Vikas H B <vikasbhapri@gmail.com>
2025-04-12 19:24:54 +0530

    Changed the next.config file to use images

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://ecommercewebapp.blob.core.windows.net/**")]
+    domains: ['ecommercewebapp.blob.core.windows.net']
   }
 };
 
